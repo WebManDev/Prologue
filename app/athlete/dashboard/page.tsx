7e9338d98845e09cd2998beb557383696313f945
WebManDev <andyhluu23@gmail.com>
2025-06-24 12:26:28 +0100

    truely fixed

---
+++ app/athlete/dashboard/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { CoachDashboard } from "@/components/coach-dashboard";
+
+export default function AthleteDashboardPage() {
+  return <CoachDashboard onLogout={() => { /* TODO: implement athlete logout */ }} />;
+} 
